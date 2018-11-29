3acb407ce5b4f6bbf79382ea4940c69c523de33e
Sasha Poma <spoma301@gmail.com>
2018-11-28 17:19:04 -0700

    started countdown

---
+++ app.js
@@ -0,0 +1,9 @@
+// countdown to December 25th 
+
+// html element grabber
+// the date for today
+//the date we want (dec 25)
+// calculation to figure out the difference in time until that day
+//display on the html
+
+//eventlistener, onchange, the chamge of the difference calculation
